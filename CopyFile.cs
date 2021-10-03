46cd5a31e9149229bf5d2e2140932b8ee63c1a58
Nir Avigad <nir290@gmail.com>
2021-10-03 19:27:09 +0300

    Add files via upload
    Copy file to a destination directory (C#)

---
+++ CopyFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp1
+{
+    class CopyFile
+    {
+        string ErrorMessage;
+        int OpStat;
+        FileInfo file;
+
+
+        public void DirectoryCopy(string sourceFileName, string destFileName)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceFileName);
+            try
+            {
+                if (!dir.Exists)
+                {
+                    OpStat = -1;
+                    ErrorMessage = "Source directory does not exist or could not be found: " + sourceFileName;
+                    return;
+                }
+
+                // Get the files in the directory and copy them to the new location.
+                file.CopyTo(destFileName);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message.ToString();
+                OpStat = -1;
+                throw;
+            }
+        }
+    }
+}
+
